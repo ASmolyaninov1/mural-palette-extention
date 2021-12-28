c34d05ccc737fe69155f2bf6d7581cb2895e6c55
ASmolyaninov <a.smolyaninov@brightlab.me>
2021-12-28 15:54:47 +0300

    add parse config

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log(`it's test console log, if it works i will pounce in window`);
+const express = require('express');
+const { default: ParseServer } = require('parse-server');
+
+const parseConfig = {
+  appId: '6e66d7ca36d7c271801bdada14bc9490',
+  masterKey: '2d8fcc6094438a0246358cf8142e00d3',
+  cloud: "./parse/cloud/index",
+  serverURL: 'https://dockerhost.forge-parse-server.c66.me:40123/parse',
+  publicServerURL: 'mural-palette-extention.tk.getforge.io',
+};
+
+module.exports.parseConfig = parseConfig;
+module.exports.URL_SITE = 'mural-palette-extention.tk.getforge.io';
+
+
+const parseServer = new ParseServer(parseConfig);
+const app = new express();
+app.use('/parse', parseServer.app);
